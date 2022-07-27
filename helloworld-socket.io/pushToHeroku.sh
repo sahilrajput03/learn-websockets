8df379dbbf99871a388f49ddf0543c85242f475d
Sahil Rajput <sahilrajput03@gmail.com>
2022-07-27 14:33:06 +0530

    Add couple of elements to make potential messages work.

--- helloworld-socket.io/pushToHeroku.sh
+++ helloworld-socket.io/pushToHeroku.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 set -x
+echo +MAKE COMMIT AND PUSH IF STAGING AREA HAS CHANGES
 git add -A; git commit -m 'chore:push to heroku'; git push
+
+echo +PUSH TO HEROKU
 git push -f heroku main
