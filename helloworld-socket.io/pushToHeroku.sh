885514e89804aa6f6164e6ef427be0a002205e7a
Sahil Rajput <sahilrajput03@gmail.com>
2022-07-27 15:54:50 +0530

    Update pushToHeoku.

--- helloworld-socket.io/pushToHeroku.sh
+++ helloworld-socket.io/pushToHeroku.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# PLEASE NOTE THAT this build only worked when I placed `Procfile` in this very folder instead of keeping in the root of repository.
 set -x
 echo +MAKE COMMIT AND PUSH IF STAGING AREA HAS CHANGES
 git add -A; git commit -m 'chore:push to heroku'; git push
