ec89d9523761e783a5c7c2aab3c79dfc8ddf822a
Sahil Rajput <sahilrajput03@gmail.com>
2022-07-27 20:22:08 +0530

    chore:push to heroku

--- helloworld-socket.io/setupNestedHeroku.sh
+++ helloworld-socket.io/setupNestedHeroku.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-heroku buildpacks:clear
-heroku buildpacks:set https://github.com/timanovsky/subdir-heroku-buildpack
-heroku buildpacks:add heroku/nodejs
-heroku config:set PROJECT_PATH=helloworld-socket.io
+source ~/.bash_functions
+# heroku.setSubDirectoryBuildpack helloworld-socket.io
+heroku.setSubDirectoryBuildpack helloworld-
