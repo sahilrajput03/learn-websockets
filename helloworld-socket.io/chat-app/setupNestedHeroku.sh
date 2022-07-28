ff66675f597238c6d2def83d48a312f61238214f
Sahil Rajput <sahilrajput03@gmail.com>
2022-07-28 13:50:29 +0530

    Fix the setupNestedHeroku command.

--- helloworld-socket.io/chat-app/setupNestedHeroku.sh
+++ helloworld-socket.io/chat-app/setupNestedHeroku.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source ~/.bash_functions
-heroku.setSubDirectoryBuildpack helloworld-socket.io
+heroku.setSubDirectoryBuildpack helloworld-socket.io/chat-app
 # heroku.setSubDirectoryBuildpack helloworld-
