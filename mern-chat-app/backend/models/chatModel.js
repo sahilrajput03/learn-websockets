2b55ae9dd3f7feee3756b3c8800081b489c41aa3
Sahil Rajput <sahilrajput03@gmail.com>
2022-11-22 00:07:05 +0530

    Add mern-chat-app project

---
+++ mern-chat-app/backend/models/chatModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const chatModel = mongoose.Schema(
+  {
+    chatName: { type: String, trim: true },
+    isGroupChat: { type: Boolean, default: false },
+    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    latestMessage: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+    },
+    groupAdmin: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  },
+  { timestamps: true } // The timestamps option tells Mongoose to assign `createdAt` and `updatedAt` fields to your schema. The type assigned is Date.
+);
+
+const Chat = mongoose.model("Chat", chatModel);
+
+module.exports = Chat;
